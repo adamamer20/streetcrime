05a217a49bfc1a0416b02259b3e46367cb4a307e
Adam Amer <136176500+adamamer20@users.noreply.github.com>
2024-01-04 23:20:55 +0100

    modify setup for package installation

--- setup.py
+++ setup.py
@@ -1,12 +1,11 @@
 from setuptools import find_packages, setup
 
-from setuptools import setup
-
 setup(
     name="streetcrime",
-    packages=find_packages(),
+    packages=find_packages(where="src"),
+    package_dir={"": "src"},
     version="1.0",
-    description="Street Theft and Robbery in Milan",
+    description="An extension to the Mesa agent-based modeling framework using Pandas DataFrames for enhanced performance, applied to the study of Street Theft and Robbery",
     author="Adam Amer",
-    license="",
+    license="MIT License",
 )
