0592366aea6bf78291f1db25577006c4f4d0085a
Adam Amer <136176500+adamamer20@users.noreply.github.com>
2024-01-05 10:07:43 +0100

    refactored model.py to work with mesa_frames and added documentation

--- src/streetcrime/model.py
+++ src/streetcrime/model.py
@@ -1,122 +1,178 @@
-import datetime as dt  # To keep track of the time of the simulation
-import os
-import sys  # To get a class from a string
-import time  # To measure the time of the loading of the files
-import uuid
-from datetime import datetime, timedelta
-from random import getrandbits
-from typing import Any
+import math
+from datetime import date, datetime, timedelta
 
 import geopandas as gpd
-import mesa
-import networkx as nx
 import numpy as np
-import osmnx as ox
 import pandas as pd
-from scipy.stats import skewnorm
-from shapely.geometry import Point
+from mesa_frames.model import ModelDF
+from scipy import stats
 
-from streetcrime.agents.criminal import Pickpocket, Robber
 from streetcrime.agents.mover import Mover
-from streetcrime.agents.police_agent import PoliceAgent
-from streetcrime.agents.worker import Worker
 from streetcrime.space.city import City
 
 
-class StreetCrime(mesa.Model):
-    Mover.walking_speed = 1.6 #m/s
-    resident_mean_resting_start_time: int = 21
-    resident_sd_resting_start_time: int = 2
-    resident_mean_resting_end_time: float = 7.5
-    resident_sd_resting_end_time: float = 0.83
-    worker_mean_work_start_time: int = 8
-    worker_sd_work_start_time: int = 2
-    worker_min_work_start_time: int = 5
-    worker_mean_work_end_time: int = 18
-    worker_sd_work_end_time: int = 2
-    worker_max_work_end_time: int = 21
-    worker_mean_self_defence: float = 0.5
-    worker_sd_self_defence: float = 0.17
-    worker_p_information: float = 0.5
-    criminal_opportunity_awareness: int = 150
-    criminal_crowd_effect: float = 0.01
-    criminal_p_information: float = 1
-    policeagent_p_information: float = 1
-    
-    def __init__(self, 
-                 space : City,
-                 p_agents = {
-                    Pickpocket: 0.07,
-                    PoliceAgent: 0.05,
-                    Robber: 0.03,
-                    Worker: 0.85},
-                 days: int = 7,
-                 len_step: int = 10,
-                 start_datetime: dt.datetime = dt.datetime(2020, 1, 1, 5, 30),
-                 num_movers: int = 100,
-                 day_act_start: int = 8,
-                 day_act_end: int = 19,
-                 **kwargs):
-        
-        self._check_parameters_validity(p_agents)
-        
+class StreetCrime(ModelDF):
+    """The StreetCrime class contains the model of the simulation.
+
+    Parameters:
+    -----------
+    space : City
+        The city where the simulation takes place.
+    p_agents : dict[type[Mover], float]
+        The dictionary containing the proportion of each type of agent in the simulation.
+    n_agents : int
+        The number of agents in the simulation.
+    data_collection : str
+        *FUTURE IMPLEMENTATION*. The frequency of the data collection. It can be 'xd', 'xd', 'xh', 'weekly', 'daily', 'hourly'. Default: '2d'
+    transport : str | list[str]
+        *FUTURE IMPLEMENTATION*. The mode of transport of the agents. It can be 'on_foot', 'car', 'public_transport'. Default: 'on_foot'
+    crime_theory : str
+        *FUTURE IMPLEMENTATION*. The crime theory used in the simulation. It can be 'RAT', 'CPT', 'SCT'. Default: 'RAT'
+    days : int
+        The number of days the simulation lasts. Default: 7
+    len_step : int
+        The length of each step in minutes. Default: 10
+    start_datetime : datetime
+        The datetime when the simulation starts. Default: datetime(date.today().year, date.today().month, date.today().day, 5, 30)
+    day_act_start : int
+        The hour when activities open during the day become available. Default: 8
+    day_act_end : int
+        The hour when activities open during the day become unavailable . Default: 19
+    mean_activity_length : float
+        The mean length of an activity in minutes. Activity length is distributed as a lognorm. Default: 60
+    sd_activity_length : float
+        The standard deviation of the length of an activity in minutes. Activity length is distributed as a lognorm. Default: 20
+
+    Attributes:
+    -----------
+    space : City
+        The city where the simulation takes place.
+    activity_len_distr : scipy.stats.lognorm
+        The lognorm distribution of the length of an activity.
+    datetime : datetime
+        The current datetime of the simulation.
+    len_step : int
+        The length of each step in minutes.
+    day_act_start : int
+        The hour when activities open during the day become available .
+    day_act_end : int
+        The hour when activities open during the day become unavailable .
+    mean_activity_length : float
+        The mean length of an activity in minutes.
+    sd_activity_length : float
+        The standard deviation of the length of an activity in minutes.
+    paths : pd.DataFrame
+        The dataframe containing current paths for agents in movement.
+    crimes : gpd.GeoDataFrame
+        The dataframe containing crimes committed during the simulation.
+    """
+
+    def __init__(
+        self,
+        space: City,
+        p_agents: dict[type[Mover], float],
+        n_agents: int = 100,
+        data_collection: str = "2d",  # TODO: implement different data collection frequencies (xw, xd, xh, weekly, daily, hourly)
+        transport: str
+        | list[
+            str
+        ] = "on_foot",  # TODO: implement other modes of transport 'car', 'public_transport'
+        crime_theory: str = "RAT",  # TODO: implement other crime theories
+        len_step: int = 10,  # minutes
+        start_datetime: datetime = datetime(
+            date.today().year, date.today().month, date.today().day, 5, 30
+        ),
+        day_act_start: int = 8,
+        day_act_end: int = 19,
+        mean_activity_length: float = 60,  # minutes
+        sd_activity_length: float = 20,  # minutes
+    ):
+        """Initializes the StreetCrime class.
+
+        Parameters:
+        -----------
+        space : City
+            The city where the simulation takes place.
+        p_agents : dict[type[Mover], float]
+            The dictionary containing the proportion of each type of agent in the simulation.
+        n_agents : int
+            The number of agents in the simulation.
+        data_collection : str
+            *FUTURE IMPLEMENTATION*. The frequency of the data collection. It can be 'xd', 'xd', 'xh', 'weekly', 'daily', 'hourly'. Default: '2d'
+        transport : str | list[str]
+            *FUTURE IMPLEMENTATION*. The mode of transport of the agents. It can be 'on_foot', 'car', 'public_transport'. Default: 'on_foot'
+        crime_theory : str
+            *FUTURE IMPLEMENTATION*. The crime theory used in the simulation. It can be 'RAT', 'CPT', 'SCT'. Default: 'RAT'
+        len_step : int
+            The length of each step in minutes. Default: 10
+        start_datetime : datetime
+            The datetime when the simulation starts. Default: datetime(date.today().year, date.today().month, date.today().day, 5, 30)
+        day_act_start : int
+            The hour when activities open during the day become available . Default: 8
+        day_act_end : int
+            The hour when activities open during the day become unavailable . Default: 19
+        mean_activity_length : float
+            The mean length of an activity in minutes. Activity length is distributed as a lognorm. Default: 60
+        sd_activity_length : float
+            The standard deviation of the length of an activity in minutes. Activity length is distributed as a lognorm. Default: 20
+        """
+        super().__init__(space)
+
+        # Initialize model attributes
+        self.activity_len_distr = stats.lognorm(
+            s=np.sqrt(np.log(sd_activity_length**2 / mean_activity_length**2 + 1)),
+            scale=math.exp(
+                np.log(mean_activity_length)
+                - 0.5 * np.log(sd_activity_length**2 / mean_activity_length**2 + 1)
+            ),
+        )
         self.space = space
-        self.space.buildings['yesterday_visits'] = 1
-        self.space.buildings['yesterday_crimes'] = 1
-        self.space.buildings['yesterday_police'] = 1
-        #TODO: Add mean_income to neighborhoods processing
-        self.space.buildings['mean_income'] = 1
-        self.space.buildings['run_visits'] = 1
-        self.space.buildings['run_police'] = 1
-        self.space.buildings['run_crimes'] = 1
-        self.days = days
         self.len_step = len_step
         self.datetime = start_datetime
-        self.num_movers = num_movers
         self.day_act_start = day_act_start
         self.day_act_end = day_act_end
-        self.seed = getrandbits(128)
-        self.run_id = uuid.UUID(int=self.seed, version=4)
-        self.n_steps = int(self.days * 24 * 60 / self.len_step)
-        self.schedule = mesa.time.RandomActivation(self)
-        if p_agents is not None:
-            self.p_agents = p_agents
-            self.p_agents['Worker'] = 1 - sum(p_agents.values())
-            self.movers = self._create_movers()
-            self.agents_info = self._create_agents_info()
-        self.model_info = pd.DataFrame(
-            1,
-            index = self.space.neighborhoods.index,
-            columns = ['yesterday_visits', 'today_visits', 'run_visits',
-                      'yesterday_crimes', 'today_crimes', 'run_crimes',
-                      'yesterday_police', 'today_police', 'run_police'],
-        )
-        #TODO: implement agents_info
-        self.crimes = gpd.GeoDataFrame(columns=['step', 'datetime', 'geometry', 'neighborhood',
-                                        'victim', 'criminal', 'witnesses', 'successful'])
-        
-        for key, value in kwargs.items():
-            setattr(self, key, value)
-        
 
-    def step(self) -> None:
-        """Advance the model by one step, updating agents_info at midnight each day and executing the step method of each agent.
-        """
+        # Create movers
+        self.create_agents(n_agents, p_agents)
+        self.paths = pd.DataFrame(
+            np.nan,
+            index=self.agents.index,
+            columns=["ancestor", "successor", "length", "travelled_distance"],
+        )
+
+        # self.agents_info = self._create_agents_info()
+
+        self.crimes = gpd.GeoDataFrame(
+            columns=[
+                "type",
+                "datetime",
+                "geometry",
+                "neighborhood",
+                "victim",
+                "criminal",
+                "witnesses",
+                "successful",
+            ]
+        )
+
+    def step(self, merged_mro=True) -> None:
+        """Advance the model by one step, updating datetime by len_step and executing the step method of each agent."""
         # Advance time
-        self.datetime = self.datetime + \
-            timedelta(minutes=self.len_step)
-                    
-        # Only at midnight of each day after the first one, update information on crimes and visits of the previous day
+        self.datetime = self.datetime + timedelta(minutes=self.len_step)
+
+        super().step(merged_mro=True)
+
+        """# Only at midnight of each day after the first one, update information on crimes and visits of the previous day
         if (self.datetime.day > 1 and self.datetime.hour == 0 and self.datetime.minute == 0):
             self._update_information()
-            self.space.save_cache_files(['roads', 'public_transport'])
-        
-        #Execute the step of the agents   
-        self.movers.apply(lambda x: x.agent.step(), axis=1)
-        
-        # Randomly compute step for each agent
-        #self.schedule.step()
+            self.space.save_paths_files(['roads', 'public_transport'])"""
 
+    def run_model(self, days: int, merged_mro=True) -> None:
+        n_steps = int(days * 24 * 60 / self.len_step)
+        super().run_model(n_steps, merged_mro=True)
+
+    ''' #TODO: implement data collector in mesa_frames
+    
     def get_data(self) -> dict[str, pd.DataFrame]:
         """
         Returns a dictionary with dataframes of the data of the model, the parameters  and the agents.
@@ -132,7 +188,7 @@ class StreetCrime(mesa.Model):
             'run_id': self.run_id,
             'model_params' : self.model_params,
             'agents_params' : self.agents_params,
-            'movers': self.movers,
+            'movers': self.agents,
             'model_params': None,
             'agents_params': None
         }]).set_index('run_id')
@@ -145,19 +201,31 @@ class StreetCrime(mesa.Model):
         else:
             data.at[self.run_id, 'agents_params'] = pd.DataFrame([self.agents_params.value])
         return data
+        
+    def _initialize_data_collection(self, how="2d") -> None:
+        """Initializes the data collection of the model.
 
-    #TODO: implement agents_params, model_params checks
-    def _check_parameters_validity(self, p_agents) -> None:
-        p_tot = 0
-        if p_agents:
-            for agent, p in p_agents.items():
-                if p < 0 or p > 1:
-                    raise ValueError(f"Proportion of {agent} should be between 0 and 1")
-                else:
-                    p_tot += p
-                if p_tot > 1:
-                    raise ValueError(f"Sum of proportions of agents should be less than 1")                    
-    
+        Parameters
+        ----------
+        how : str
+            The frequency of the data collection. It can be 'xd', 'xd', 'xh', 'weekly', 'daily', 'hourly'.
+        """
+        # TODO: finish implementation of different data collections
+        if how == "2d":
+            categories = [
+                "yesterday_visits",
+                "today_visits",
+                "run_visits",
+                "yesterday_crimes",
+                "today_crimes",
+                "run_crimes",
+                "yesterday_police",
+                "today_police",
+                "run_police",
+            ]
+        return {key: 0 for key in categories}'''
+
+    '''#TODO: implement information in refactored version
     def _update_information(self) -> None:
         """Updates 'yesterday_crimes' and 'yesterday_visits' columns of the self.buildings with data
         collected in today + '_crimes' and today + '_visits' columns of the self.neighborhoods dataframe.
@@ -192,40 +260,8 @@ class StreetCrime(mesa.Model):
         self.movers_info = self.__set_date_columns(self.datetime.date(), self.movers_info)
         
         self.movers_info = self.movers_info.astype(pd.SparseDtype(float, np.nan))
-
-    def _create_movers(self) -> pd.DataFrame():
-        """Creates the movers of the model and adds them to the schedule and the space
-        
-        Parameters
-        ----------
-        num_movers : int
-            The number of movers to create
-        movers : dict[type[Mover], float]
-            The dictionary of movers to create. The keys are the types of movers and the values are the percentages of each mover type to create.
-        """
-        start_time = time.time()
-        print("Creating movers: ...")
-        
-        movers = pd.DataFrame(columns = ['id', 'type', 'agent']) #TODO: implement all columns
-
-        movers['id'] = [uuid.uuid4().int for _ in range(self.num_movers)]
-        movers_type = np.array([])
-        for mover, p in self.p_agents.items():
-            movers_type = np.concatenate((movers_type, np.full(int(self.num_movers*p), mover)))
-        movers['type'] = movers_type
-        movers['agent'] = movers.apply(lambda x: x.type(unique_id = x.id, 
-                                                        model = self, 
-                                                        geometry=None, 
-                                                        crs=self.space.crs), axis=1)
-        movers['agent'].apply(self.schedule.add)
-        
-        self.space.add_agents(movers['agent'].to_list())
-        
-        print("Created movers: " + "--- %s seconds ---" % (time.time() - start_time))
-        return movers
-    
-    def _create_agents_info(self) -> pd.DataFrame():
-        idx = pd.MultiIndex.from_product([self.movers.id, self.space.neighborhoods.index], names=['mover_id', 'neighborhood_id'])
+        def _create_agents_info(self) -> pd.DataFrame():
+        idx = pd.MultiIndex.from_product([self.agents.id, self.space.neighborhoods.index], names=['mover_id', 'neighborhood_id'])
         columns = ['yesterday_visits', 'today_visits', 'run_visits', 
                    'yesterday_crimes', 'today_crimes', 'run_crimes',
                    'yesterday_police', 'today_police', 'run_police']
@@ -234,17 +270,4 @@ class StreetCrime(mesa.Model):
         #TO KEEP EVERY DAY (Adding today columns)
         #agents_info = self.__set_date_columns(self.datetime.date(), agents_info)
         
-        return agents_info
-
-    def __set_date_columns(self, date : dt.date, df : pd.DataFrame) -> pd.DataFrame:
-        date = str(date)
-        df.loc[0, date + '_visits'] = 1
-        df.loc[0, date + '_crimes'] = 1
-        df.loc[0, date + '_police'] = 1
-        df[date + '_visits'] = df[date + '_visits'].astype(pd.SparseDtype(float, np.nan))
-        df[date + '_crimes'] = df[date + '_crimes'].astype(pd.SparseDtype(float, np.nan))
-        df[date + '_police'] = df[date + '_police'].astype(pd.SparseDtype(float, np.nan))
-        return df
-        
-            
-        
+        return agents_info'''
