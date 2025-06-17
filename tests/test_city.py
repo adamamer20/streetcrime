bc2786a3833c426e2d116f1e7c82835c72034ff5
Adam Amer <136176500+adamamer20@users.noreply.github.com>
2025-06-17 11:05:01 +0200

    Refactor test files to improve structure and readability; update city and model tests to use new data loading methods

--- tests/test_city.py
+++ tests/test_city.py
@@ -1,16 +1,18 @@
-import unittest
-import geopandas as gpd
-import osmnx as ox
-from streetcrime.space.city import City
 import os
+import unittest
+
+import geopandas as gpd
 import networkx as nx
+import osmnx as ox
+
+from streetcrime.space.city import City
+
 
 class TestCity(unittest.TestCase):
-    def setUp(self) -> None:   
-        self.city = City(crs= 'epsg:4326',
-                         city_name= 'Duomo, Milan, Italy')
-        
-    '''
+    def setUp(self) -> None:
+        self.city = City(crs="epsg:32632", city_name="Duomo, Milan, Italy")
+
+    """
         # Test with invalid crs
         out_file = "test_roads.graphml"
         city_name = "Milan, "
@@ -36,8 +38,8 @@ class TestCity(unittest.TestCase):
         tolerance = 10
         traffic_factor = 2
         with self.assertRaises(ValueError):
-            obtaining_roads(out_file, city_name, crs, tolerance, traffic_factor)'''
-        
+            obtaining_roads(out_file, city_name, crs, tolerance, traffic_factor)"""
+
     def test_obtaining_roads(self):
         # Test with .graphml file format
         out_file = "test_roads.graphml"
@@ -45,7 +47,7 @@ class TestCity(unittest.TestCase):
         crs = "epsg:4326"
         tolerance = 10
         traffic_factor = 2
-        self.city.obtaining_roads(out_file, city_name, crs, tolerance, traffic_factor)
+        self.city.load_data(tolerance, traffic_factor, out_file)
         self.assertTrue(os.path.isfile(out_file))
         roads = ox.load_graphml(out_file)
         self.assertIsInstance(roads, nx.MultiDiGraph)
@@ -53,15 +55,14 @@ class TestCity(unittest.TestCase):
 
         # Test with .gpkg file format
         out_file = "test_roads.gpkg"
-        self.city.obtaining_roads(out_file, city_name, crs, tolerance, traffic_factor)
+        self.city.load_data(tolerance, traffic_factor, out_file)
         self.assertTrue(os.path.isfile(out_file))
-        roads_nodes = gpd.read_file(out_file, layer = 'nodes').set_index('osmid')
-        roads_edges = gpd.read_file(out_file, layer = 'edges').set_index(['u', 'v', 'key'])
+        roads_nodes = gpd.read_file(out_file, layer="nodes").set_index("osmid")
+        roads_edges = gpd.read_file(out_file, layer="edges").set_index(["u", "v", "key"])
         roads = ox.graph_from_gdfs(roads_nodes, roads_edges)
         self.assertIsInstance(roads, nx.MultiDiGraph)
         os.remove(out_file)
-    
-if __name__ == '__main__':
+
+
+if __name__ == "__main__":
     unittest.main()
-    
-    
--- tests/test_model.py
+++ tests/test_model.py
@@ -1,22 +1,25 @@
 import unittest
+
 from streetcrime.model import StreetCrime
 from streetcrime.space.city import City
 
+
 class TestModel(unittest.TestCase):
     def test_model_init(self):
-        StreetCrime(space = City(crs = 'epsg:7791',
-                                 roads = 'tests/data/processed/roads.gpkg',
-                                 public_transport = 'tests/data/processed/public_transport.gpkg',
-                                 neighborhoods = 'tests/data/processed/neighborhoods.gpkg',
-                                 buildings = 'tests/data/processed/buildings.shp'))
-    
+        city = City(crs="epsg:32632", city_name="Test City")
+        city.load_data(
+            roads_file="tests/data/processed/roads.gpkg", buildings_file="tests/data/processed/buildings.shp"
+        )
+        StreetCrime(space=city)
+
     def test_model_with_agents(self):
-        StreetCrime(space = City(crs = 'epsg:7791',
-                                 roads = 'tests/data/processed/roads.gpkg',
-                                 public_transport = 'tests/data/processed/public_transport.gpkg',
-                                 neighborhoods = 'tests/data/processed/neighborhoods.gpkg',
-                                 buildings = 'tests/data/processed/buildings.shp'),
-                    num_movers=100)
-    
-if __name__ == '__main__':
+        city = City(crs="epsg:32632", city_name="Test City")
+        city.load_data(
+            roads_file="tests/data/processed/roads.gpkg", buildings_file="tests/data/processed/buildings.shp"
+        )
+        model = StreetCrime(space=city)
+        model.create_agents(n_agents=100, p_agents={})
+
+
+if __name__ == "__main__":
     unittest.main()
